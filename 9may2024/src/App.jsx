e47b2caa2f41aa1bc0fdf86ee5d4785ddf729adb
Edward Vargas <edthevargas@gmail.com>
2024-05-10 22:31:00 -0400

    added placeholder about section

--- 9may2024/src/App.jsx
+++ 9may2024/src/App.jsx
@@ -1,7 +1,43 @@
 import "./App.css";
 
 function App() {
-   return <>Edward's Portfolio</>;
+   return (
+      <>
+         <h1>Edward Vargas</h1>
+         <h2>Software Engineer</h2>
+         <h3>
+            I see myself as a puzzle solver and I see software as a puzzle that is the most
+            satisfying to bring to it's conclusion
+            <br />
+            Passionate software enthusiast with a relentless drive for learning and innovation.
+            <br />
+            Eager to embark on a journey in the dynamic world of software engineering,
+            <br />
+            leveraging self-motivation and adaptability as key assets in every project.
+         </h3>
+         {/* <div class="about"> I've always loved a challenge and with software that comes with the space. </div> */}
+         <div class="about">
+            Passionate software enthusiast with a relentless drive for learning and innovation.
+            <br />
+            Eager to embark on a journey in the dynamic world of software engineering,
+            <br />
+            leveraging self-motivation and adaptability as key assets in every project.
+            <br />
+            In my off time I love to create with my hands. Whether thats fashion or
+            electronics.
+         </div>
+         <div class="projects">
+            <h1>Projects</h1>
+         </div>
+         <div class="experience">
+            <h1>Experience</h1>
+         </div>
+         <div class="connect">github linkedin</div>
+         <div class="description">
+            Coded in Visual Studio Code. Built with react.js and SASS. Deployed on cloudflare.
+         </div>
+      </>
+   );
 }
 
 export default App;
