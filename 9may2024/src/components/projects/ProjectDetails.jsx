772997d9c94708a2af180529030a4f87167d21ac
Edward Vargas <edthevargas@gmail.com>
2024-05-19 22:36:09 -0400

    improved layout

--- 9may2024/src/components/projects/ProjectDetails.jsx
+++ 9may2024/src/components/projects/ProjectDetails.jsx
@@ -8,15 +8,15 @@ export default function ProjectDetails({ Title, Description, Date, TechStack, UR
       <div className="projectDetails">
          <a href="#">
             <div className="projectDetails__title">
+               <div className="projectDetails__time">{Date}</div>
                <h1>{Title}</h1>
             </div>
-            <div className="projectDetails__description">{Description}</div>
             <div className="projectDetails__techstack">
                {boilerPlate.map((tool) => {
                   return <div className="tools">{tool}</div>;
                })}
             </div>
-            <div className="projectDetails__time">{Date}</div>
+            <div className="projectDetails__description">{Description}</div>
          </a>
       </div>
    );
